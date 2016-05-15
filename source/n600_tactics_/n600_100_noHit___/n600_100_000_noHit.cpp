9b19a321fd591d59886d6ad10f0528791bf52c33
muzudho <muzudho1@gmail.com>
2016-05-16 01:48:37 +0900

    スマホ用の整理続き☆ｗｗｗｗ（＾▽＾）

--- source/n600_tactics_/n600_100_noHit___/n600_100_000_noHit.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "../../../header/n600_tactics_/n600_100_noHit___/n600_100_000_noHit.h"
-
-
-
-
-NoHit::NoHit()
-{
-	this->unuse = 0;
-};
--- source/n600_tactics_/n600_200_hit_____/n600_200_000_hit.cpp
+++
@@ -1,8 +0,0 @@
-﻿#include "../../../header/n600_tactics_/n600_200_hit_____/n600_200_000_hit.h"
-
-
-
-Hit::Hit()
-{
-	this->unuse = 0;
-};
